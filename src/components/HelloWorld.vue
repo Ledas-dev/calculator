2360d86bd47a5b156810f6fc10780afb10707b44
Lagumir <Legals92@mail.ru>
2020-11-01 01:20:17 +0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,187 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <span class="text">Calculator</span>
+    <form class="calculator"  v-on:submit.prevent>
+      <input @keyup.enter="summ"
+        type="text" placeholder="0" class="input-num" v-model="result">
+      <button type="button" name="button" class="item col-1" @click="minusNum()">CE</button>
+      <button type="button" name="button" class="item col-1" @click="zero()">C</button>
+      <button type="button" name="button" class="item col-1" @click="minus()">D</button>
+      <button
+        type="button"
+        name="button"
+        class="operation col-1"
+        v-for="(oper, index) in operation"
+        :key="index"
+        @click="input(oper)"
+      >
+        {{ oper }}
+      </button>
+      <button
+        type="button"
+        name="button"
+        class="operation col-1"
+        @click="summ()"
+      >=</button>
+    </form>
+    <div class="num-cal">
+      <button
+        type="button"
+        name="button"
+        class="number"
+        v-for="(num, index) in number"
+        :key="index"
+        :class="{'col-1': num.isActiv}"
+        @click="input(num.x)"
+      >
+        {{ num.x }}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      number:[
+        {x:1, isActiv:false},
+        {x:2, isActiv:false},
+        {x:3, isActiv:false},
+        {x:4, isActiv:false},
+        {x:5, isActiv:false},
+        {x:6, isActiv:false},
+        {x:7, isActiv:false},
+        {x:8, isActiv:false},
+        {x:9, isActiv:false},
+        {x:'+-', isActiv:true},
+        {x:0, isActiv:false},
+        {x:'.', isActiv:true}
+      ],
+      operation:['+','-','/','*'],
+      result:''
+    }
+  },
+  methods:{
+    input(char){
+        this.result = this.result.toString()
+        this.result += char + ''
+    },
+    zero(){
+      this.result = ''
+    },
+    minus(){
+      this.result = this.result.substring(0, this.result.length - 1)
+    },
+    summ(){
+      event.preventDefault()
+      this.result = eval(this.result)
+    },
+    minusNum(){
+      if(Number(this.result)){
+        this.result = ''
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calculator{
+  width: 351px;
+  height: 371px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
 }
-ul {
-  list-style-type: none;
+.input-num{
+  width: 99%;
+  height: 84px;
+  border: none;
+  font-size: 30px;
+  outline: 0;
+  background-color: #E4E4E4;
+  grid-column: 1/-1;
+  text-align: right;
+}
+.text{
+  width: 351px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 28px;
+  line-height: 33px;
+  text-align: center;
+}
+.operation{
+  grid-column: 5/4;
+  width: 100%;
+  height: 84px;
+  padding: 20px;
+  text-align: center;
+  border: 1px solid #E4E4E4;
+  outline: none;
+  background: #fff;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 28px;
+}
+.col-1:hover{
+  background: #E5E5E5;
+}
+.number{
+  width: 100%;
+  height: 84px;
+  padding: 20px;
+  text-align: center;
+  border: 1px solid #E4E4E4;
+  outline: none;
+  background: #fff;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 28px;
+  transition: 0.2s;
+}
+.number:hover{
+  background: #E4E4E4;
+}
+button{
   padding: 0;
+  margin: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.item{
+  width: 100%;
+  height: 84px;
+  padding: 20px;
+  text-align: center;
+  border: 1px solid #E4E4E4;
+  outline: none;
+  background: #fff;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 28px;
 }
-a {
-  color: #42b983;
+.col-1{
+  background: #F3F3F3;
+    transition: 0.2s;
+}
+.decision{
+  margin-right: 20px;
+  font-size: 18px;
+line-height: 21px;
+}
+.answer{
+  margin-right: 20px;
+  margin-top: -20px;
+  font-size: 32px;
+}
+.num-cal{
+  width: 265px;
+  position: relative;
+  top:-201px;
+  left:605px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
